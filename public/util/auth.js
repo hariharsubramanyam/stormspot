5ad41b44116fbb10080568a1b2d3c448a2570b3b
Harihar Subramanyam <hahasubbu@hotmail.com>
2014-10-25 12:30:58 -0400

    Added functions on client side to talk to auth API

--- public/util/auth.js
+++ public/util/auth.js
@@ -2,6 +2,9 @@
  * This file defines functions for communicating with the authentication API.
  */
 (function() {
+  var set_session_id = function(session_id) {
+    $.cookie("session_id", session_id, {"path": "/"});
+  };
   Global.auth = {};
 
   Global.auth.register = function(username, password, callback) {
@@ -13,14 +16,67 @@
         "password": password
       },
       "success": function(data) {
-        console.log(data);
+        data = JSON.parse(data);
+        if (data.error === null) {
+          set_session_id(data.result);
+        }
+        callback(data);
+      },
+      "error": function() {
+        callback(null);
       }
     });
   };
 
   Global.auth.login = function(username, password, callback) {
+    $.ajax({
+      "url": "/auth/login",
+      "method": "POST",
+      "data": {
+        "username": username,
+        "password": password
+      },
+      "success": function(data) {
+        data = JSON.parse(data);
+        if (data.error === null) {
+          set_session_id(data.result);
+        }
+        callback(data);
+      },
+      "error": function() {
+        callback(null);
+      }
+    });
   };
 
   Global.auth.logout = function(callback) {
+    $.ajax({
+      "url": "/auth/logout",
+      "method": "DELETE",
+      "success": function(data) {
+        data = JSON.parse(data);
+        if (data.error === null) {
+          $.removeCookie("session_id");
+        }
+        callback(data);
+      },
+      "error": function(data) {
+        callback(null);
+      }
+    });
+  };
+
+  Global.auth.validate = function(callback) {
+    $.ajax({
+      "url": "/auth/validate",
+      "method": "GET",
+      "success": function(data) {
+        data = JSON.parse(data);
+        callback(data);
+      },
+      "error": function(){
+        callback(null);
+      }
+    });
   };
 })();
