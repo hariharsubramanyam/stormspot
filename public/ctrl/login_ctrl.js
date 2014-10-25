1f8c709d4962ed15f2c9ba9705884c2859f64610
Harihar Subramanyam <hahasubbu@hotmail.com>
2014-10-25 15:00:41 -0400

    Updated ids of login control

--- public/ctrl/login_ctrl.js
+++ public/ctrl/login_ctrl.js
@@ -1,9 +1,18 @@
 (function() {
   var LoginCtrl = function(login_div_name) {
     var div = $("#"+login_div_name);
-    div.addClass("active");
-    div.html(Handlebars.templates.login());
+    var html = $(Handlebars.templates.login());
 
+    var btn_login = html.find("#btn_login");
+    var btn_register = html.find("#btn_register");
+    var txt_reg_username = html.find("#txt_reg_username");
+    var txt_reg_password = html.find("#txt_reg_password");
+    var txt_reg_confirm = html.find("#txt_reg_confirm");
+    var txt_log_username = html.find("#txt_log_username");
+    var txt_log_password = html.find("#txt_log_password");
+
+    div.html(html);
+    div.addClass("active");
     var that = {};
     return that;
   };
