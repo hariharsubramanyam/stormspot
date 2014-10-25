de3fc74e36cebf6081f85be37fb9ae2b938604f8
Harihar Subramanyam <hahasubbu@hotmail.com>
2014-10-25 10:43:31 -0400

    Get geolocation

--- public/app.js
+++ public/app.js
@@ -1,4 +1,1 @@
-Globals = {};
-$(document).ready(function() {
-  swal({   title: "Error!",   text: "Here's my error message!",   type: "error",   confirmButtonText: "Cool" });
-});
+Global = {};
--- public/util/geolocation.js
+++ public/util/geolocation.js
@@ -1,3 +1,9 @@
 (function() {
-
+  Global.getGeoLocation = function(callback) {
+    if (navigator.geolocation) {
+      return navigator.geolocation.getCurrentPosition(callback);
+    } else {
+      callback(null);
+    }
+  };
 })();
