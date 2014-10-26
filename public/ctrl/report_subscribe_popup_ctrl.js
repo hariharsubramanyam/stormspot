eea3f1a7f948a3f2af5e3871204d769ff6b4835d
Guillermo Vargas <gjvargas@mit.edu>
2014-10-26 16:21:09 -0400

    hopefully last merge resolution

--- public/ctrl/report_subscribe_popup_ctrl.js
+++ public/ctrl/report_subscribe_popup_ctrl.js
@@ -10,12 +10,7 @@
       var btn_subscribe = $("#btn_subscribe");
       var btn_report = $("#btn_report");
       btn_subscribe.click(function() {
-<<<<<<< HEAD
-        map.closePopup(popup);
-        Global.SubscribeCtrl("subscribe-div", map, e);
-=======
->>>>>>> parent of 925c7c9... subscribe button crshows form
-        console.log("subscribe");
+        Global.SubscribeCtrl("marker-box-form");
       });
       btn_report.click(function() {
         console.log("report");
