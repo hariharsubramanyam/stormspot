fe9ef07eb4d65b13b8e1fe20809a55e1f75f27c2
Guillermo Vargas <gjvargas@mit.edu>
2014-10-26 16:21:14 -0400

    Revert "subscribe button crshows form"
    This reverts commit 925c7c9f98d320ceeb73f23bed404456d862ce12.

--- public/ctrl/report_subscribe_popup_ctrl.js
+++ public/ctrl/report_subscribe_popup_ctrl.js
@@ -10,7 +10,7 @@
       var btn_subscribe = $("#btn_subscribe");
       var btn_report = $("#btn_report");
       btn_subscribe.click(function() {
-        Global.SubscribeCtrl("marker-box-form");
+        console.log("subscribe");
       });
       btn_report.click(function() {
         console.log("report");
