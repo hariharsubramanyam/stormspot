598c2c958f0fa817d6a751263a80add4f350a17d
Giancarlo Sturla <sturlag@mit.edu>
2014-10-26 18:34:23 -0400

    bug fix

--- public/ctrl/report_subscribe_popup_ctrl.js
+++ public/ctrl/report_subscribe_popup_ctrl.js
@@ -5,34 +5,19 @@
 
     //Create a marker when clicking on the map
     var onMapClick = function(e){
-
-      var finish = function(){
-        var marker = L.marker().setLatLng(e.latlng);
-        marker.on("click", function(){
-
-        });
-        marker.addTo(map);
-        //markerHTML.removeClass("active");
-        //div.html("");
-        map.closePopup(popup);
-      }
-
       popup.setLatLng(e.latlng);
       popup.openOn(map);
-      var btn_subscribe = $("#btn_subscribe", finish);
+      var btn_subscribe = $("#btn_subscribe");
       var btn_report = $("#btn_report");
       btn_subscribe.click(function() {
         Global.SubscribeCtrl("marker-box-form");
         console.log("subscribe");
       });
       btn_report.click(function() {
-        Global.ReportCtrl("marker-box-form");
+        Global.ReportCtrl("marker-box-form", popup, map, e);
         console.log("report");
       });
     };
-
-
-
     map.on("click", onMapClick);
   };
 
