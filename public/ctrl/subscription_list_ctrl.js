17ff208abfcec441db6a5c5dd32f42fcbbbd3d84
Guillermo Vargas <gjvargas@mit.edu>
2014-10-26 22:04:33 -0400

    adding subscription list files

---
+++ public/ctrl/subscription_list_ctrl.js
@@ -0,0 +1,26 @@
+/**
+ * This files defines the controller for the list.
+ */
+(function() {
+  /**
+   * Setup the list controller.
+   *
+   * @param list_div_id - The id of the div which will contain the list button.
+   * @param callback - Executed as callback() when the user logs out.
+   */
+  var SubscriptionListCtrl = function(list_div_id, callback) {
+    // Add the subscriptions button (from template) to div.
+    var div = $("#"+list_div_id);
+    var html = $(Handlebars.templates.list());
+    div.html(html);
+
+    Global.subscribe.get(function(data){
+      data.result.forEach(function(subscription){
+        var elem = $(Handlebars.templates.list_element({"subscription": subscription}));
+        html.append(elem);
+      });
+    })
+  };
+
+  Global.SubscriptionListCtrl = SubscriptionListCtrl;
+})();
